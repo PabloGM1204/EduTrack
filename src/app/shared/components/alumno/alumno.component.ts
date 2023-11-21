edeb4437e9ab643f9cea2bdac465f655f2b24760
PabloGM1204 <pgarmun203@g.educaand.es>
2023-11-21 11:12:55 +0100

    Muestra de datos del alumno

--- src/app/shared/components/alumno/alumno.component.ts
+++ src/app/shared/components/alumno/alumno.component.ts
@@ -10,11 +10,16 @@ export class AlumnoComponent  implements OnInit {
 
   @Input() alumno: Alumno | null = null;
 
-  //@Input() name: string | null = null
+  @Input() name: string | null = null;
+  @Input() fechaNacimiento: string | null = null;
+  @Input() email: string | null = null;
+
   constructor() { }
 
   ngOnInit() {
-    console.log(this.alumno)
+    console.log(this.name)
+    console.log(this.fechaNacimiento)
+    console.log(this.email)
   }
 
 }
